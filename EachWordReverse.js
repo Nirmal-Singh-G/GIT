502943722e52411b7380961e425c23196ff6b517
Nirmal-Singh-G <nirmalsinghg.10@gmail.com>
2026-04-26 11:22:45 +0530

    Third Msg

--- EachWordReverse.js
+++ EachWordReverse.js
@@ -5,3 +5,14 @@ let result = sentence
   .join(' ');
 
 console.log(result);
+//
+let str = "JavaScript";
+let count = 0;
+
+for (let char of str.toLowerCase()) {
+    if ("aeiou".includes(char)) {
+        count++;
+    }
+}
+
+console.log(count);
